3d823ee213af7940ce7ef685a876af122956a401
Angela Webb <angela.webb2003@uky.edu>
2022-04-23 17:41:56 -0400

    Javascript "dark mode"

---
+++ scripts/main.js
@@ -0,0 +1,35 @@
+if (new Date().getHours() > 18) {
+  const html = document.querySelector('html');
+  html.style.backgroundColor = 'black';
+  html.style.color = 'white';
+}
+
+if (new Date().getHours() > 18) {
+  const GridNav = document.getElementsByClassName('nav');
+  GridNav[0].style.backgroundColor = 'black';
+  GridNav[0].style.color = 'white';
+}
+
+if (new Date().getHours() > 18) {
+  const GridBody = document.getElementsByClassName('product');
+  GridBody[0].style.backgroundColor = 'black';
+  GridBody[0].style.color = 'white';
+}
+
+if (new Date().getHours() > 18) {
+  const GridBody = document.getElementsByClassName('product');
+  GridBody[1].style.backgroundColor = 'black';
+  GridBody[1].style.color = 'white';
+}
+
+if (new Date().getHours() > 18) {
+  const GridBody = document.getElementsByClassName('product');
+  GridBody[2].style.backgroundColor = 'black';
+  GridBody[2].style.color = 'white';
+}
+
+if (new Date().getHours() > 18) {
+  const GridBody = document.getElementsByClassName('product');
+  GridBody[3].style.backgroundColor = 'black';
+  GridBody[3].style.color = 'white';
+}
